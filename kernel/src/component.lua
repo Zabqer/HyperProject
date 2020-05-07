e8f88a4e715fdce85ac1164f560df17c4ee2d895
Gutnik Alexey <zabqer@gmail.com>
2020-05-07 19:58:32 +0300

    Rename

--- kernel/src/component.lua
+++ kernel/src/component.lua
@@ -8,28 +8,29 @@ local drivers = {}
 
 -- TODO by uuid
 
-addKenrelEventHandler({"signal", "component_added"}, function (type, uuid)
-	type = type:gsub("_", "-")
-	if drivers[type] then
-		if not devfs.data[type] then
+addKenrelEventHandler({"signal", "component_added"}, function (t, uuid)
+	kernelLog(Log.DEBUG, "Component added", t, uuid)
+	t = t:gsub("_", "-")
+	if drivers[t] then
+		if not devfs.data[t] then
 			local allocator, list = createAllocator(true, true)
-			drivers[type].allocator = allocator
-			devfs.data[type] = list
+			drivers[t].allocator = allocator
+			devfs.data[t] = list
 		end
-		local d = drivers[type].allocator:new()
+		local d = drivers[t].allocator:new()
 		d.__file = true
 		d.uuid = uuid
-		for k, v in pairs(drivers[type].add_callback()) do
+		for k, v in pairs(drivers[t].add_callback()) do
 			d[k] = v
 		end
-		kernelLog(Log.DEBUG, "Created device", uuid, "at path /dev/" .. type .. "/" .. d.index)
+		kernelLog(Log.DEBUG, "Created device", uuid, "at path /dev/" .. t .. "/" .. d.index)
 	end
 end)
 
-function register_driver(type, add_callback, remove_callback)
-	kernelLog(Log.DEBUG, "Register driver for", type)
-	type = type:gsub("_", "-")
-	drivers[type] = {
+function register_driver(t, add_callback, remove_callback)
+	kernelLog(Log.DEBUG, "Register driver for", t)
+	t = t:gsub("_", "-")
+	drivers[t] = {
 		add_callback = add_callback,
 		remove_callback = remove_callback
 	}
